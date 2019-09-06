6ef5b58e3d8bf6c55e3354a3edfde917095f4024
SmallHeat <1206840957@qq.com>
2019-09-06 19:20:52 +0800

    更新

---
+++ Car/Business/Workbench/Cell/RepaidCell.h
@@ -0,0 +1,21 @@
+//
+//  UnpaidCell.h
+//  Car
+//
+//  Created by mac on 2019/9/2.
+//  Copyright © 2019 SmallHeat. All rights reserved.
+//  已回款cell
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RepaidCell : UITableViewCell
+
++(float)cellHeight;
+
+-(void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
